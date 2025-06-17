b3cab689ea81b8ddf4676e26ff9a35af03b6223d
vitorlacerda05 <vitorlacerda05@gmail.com>
2025-06-17 13:57:28 -0300

    Refactor login screen layout for improved structure and user experience; introduce SelectAccountTypeScreen for account type selection with enhanced UI elements and logo integration.

--- lib/screens/login-configure/select_account_type_screen.dart
+++ lib/screens/login-configure/select_account_type_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import '../login/logo_clinicasa.dart';
+import '../../widgets/logo_clinicasa.dart';
 
 class SelectAccountTypeScreen extends StatelessWidget {
   const SelectAccountTypeScreen({super.key});
